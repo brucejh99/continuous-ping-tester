db9cf2b3c9ca443191487374b4b685d2e69498bc
Bruce Hong <32785837+brucejh99@users.noreply.github.com>
2020-06-03 19:59:37 -0400

    Improved error handling

--- pingTest.js
+++ pingTest.js
@@ -4,21 +4,24 @@ const ping = require('net-ping');
 const { timeDiff } = require('./utils/time');
 
 const HOST = '104.160.131.3';
-const TIMEOUT = 500;
+const TIMEOUT = 2000;
 
 const options = {
     timeout: TIMEOUT
 };
-var maxTime = 0, minTime = TIMEOUT, serverErrors = 0, socketErrors = 0; tests = 0, pingSum = 0;
+var maxTime = 0, minTime = TIMEOUT, serverErrors = 0, socketErrors = 0, tests = 0, pingSum = 0;
 var errorTimes = [];
 const startTime = Date.now();
 
-const session = ping.createSession(options);
+var session = ping.createSession(options);
 session.on('error', function() {
     const errorTime = Date.now();
     console.log(`Error pinging ${HOST} at ${new Date(errorTime).toUTCString()}`);
     socketErrors++;
     errorTimes.push(errorTime);
+    // reset session
+    session.close();
+    session = ping.createSession(options);
 });
 
 keypress(process.stdin);
@@ -27,13 +30,13 @@ const pinghost = function() {
     session.pingHost(HOST, function(err, target, reqTime, resTime) {
         var time = resTime - reqTime;
         pingSum += time;
-        if (time < minTime) minTime = time;
-        if (time > maxTime) maxTime = time;
         if (err) {
             serverErrors++;
             errorTimes.push(reqTime);
             console.log(`Error received at ${new Date(resTime).toUTCString()} from request at ${new Date(reqTime).toUTCString()}`);
         } else {
+            if (time < minTime) minTime = time;
+            else if (time > maxTime) maxTime = time;
             console.log(`Pinged ${target} in ${time}ms`);
         }
         tests++;
@@ -49,10 +52,10 @@ process.stdin.on('keypress', () => {
     console.log(`Min ping: ${minTime}ms.`);
     console.log(`Average ping: ${Math.round((pingSum * 100) / tests) / 100}ms.`);
     console.log(`${socketErrors + serverErrors}/${tests} failed.`);
-    console.log(`Socket errors (might be because of your computer or internet connection): ${socketErrors}.`);
-    console.log(`Server errors (might be out of your control): ${serverErrors}.`);
+    console.log(`Socket errors: ${socketErrors}.`);
+    console.log(`Server errors: ${serverErrors}.`);
     if (socketErrors || serverErrors) console.log('Errors occurred at:');
-    errorTimes.forEach(time => console.log(time));
+    errorTimes.forEach(time => console.log(new Date(time).toUTCString()));
     session.close();
     process.exit();
 });
